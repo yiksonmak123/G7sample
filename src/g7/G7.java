7f8e3f6c00b0aada13b7ccbb3b1dfa0492341a07
Tarc <Tarc@B104>
2018-11-01 10:44:29 +0800

    updated my friend story #213621

--- src/g7/G7.java
+++ src/g7/G7.java
@@ -6,6 +6,7 @@ public class G7 {
     public static void main(String[] args) {
         System.out.println("Hello fellow!");
         System.out.println("Hello world!");
+            System.out.println("fuck you");
     }
     
 }
